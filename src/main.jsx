d44627839636a034cb9ee67bf4109c0e40b55161
Raw199God <rishitmathur5f@gmail.com>
2025-12-21 11:46:54 +0530

    Basic App Setup

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Home from './components/Home.jsx'
+
+const router = createBrowserRouter([{
+  path:'/',
+  element:<App />,
+  children:[
+    {
+      index:true , element:<Home/>
+    }
+  ]
+}])
+createRoot(document.getElementById('root')).render(
+    <>
+    <RouterProvider router = {router} >
+    <App />
+    </RouterProvider>
+    </>
+
+)
