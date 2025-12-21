1db811ca0e3d49b02bcea2a08b6c5ce65d754fc6
Raw199God <rishitmathur5f@gmail.com>
2025-12-21 12:02:16 +0530

    Setup Layout for App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from 'react-router'
 import './App.css'
 import Header from './components/Header'
+import Selector from './components/Selector'
+import Friends from './components/Friends'
 function App() {
   return (
     <>
      <Header/>
+     <div className='layout'>
+      <Selector/>
      <Outlet />
+     <Friends/>
+     </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router'
-import Home from './components/Home.jsx'
+import Dashboard from './components/Dashboard.jsx'
 
 const router = createBrowserRouter([{
   path:'/',
   element:<App />,
   children:[
     {
-      index:true , element:<Home/>
+      index:true , element:<Dashboard/>
     }
   ]
 }])
